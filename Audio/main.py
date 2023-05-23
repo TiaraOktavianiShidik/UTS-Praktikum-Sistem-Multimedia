6ced90c751eaed220cbb612a1cd78141f0f0ec60
Tiara Oktaviani Shidik <76018503+TiaraOktavianiShidik@users.noreply.github.com>
2023-05-23 17:33:23 +0700

    Update main.py

--- Audio/main.py
+++ Audio/main.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request
 import os
 import librosa
 import soundfile as sf
-# import subprocess
-from pydub import AudioSegment
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
@@ -34,9 +32,6 @@ def compress():
     # Simpan audio hasil kompresi
     sf.write(compressed_audio_path, compressed_data, sample_rate)
 
-    # Hapus file audio asli
-    # os.remove(audio_path)
-
     # Render halaman hasil kompresi dengan menampilkan file audio asli dan hasil kompresi
     return render_template('result-compress.html', original_audio=audio.filename, compressed_audio=f'compressed_{audio.filename}')
 
@@ -54,8 +49,7 @@ def process():
     # Load audio menggunakan library librosa
     audio_data, sample_rate = librosa.load(audio_path)
 
-    # Lakukan pemrosesan efek suara di sini menggunakan library dan algoritma yang sesuai
-    # Contoh: Melakukan perubahan pitch menggunakan algoritma Time Stretching pada library librosa
+    # Melakukan perubahan pitch menggunakan algoritma Time Stretching pada library librosa
     audio_stretch = librosa.effects.time_stretch(audio_data, rate=0.8)
 
     # Path untuk file audio hasil pemrosesan efek suara
@@ -64,9 +58,6 @@ def process():
 
     # Simpan file audio hasil pemrosesan
     sf.write(processed_audio_path, audio_stretch, sample_rate)
-
-    # Hapus file asli yang diunggah
-    # os.remove(audio_path)
 
     # Render halaman hasil pemrosesan dengan menampilkan file audio asli dan hasil pemrosesan
     return render_template('result-process.html', original_audio=filename, processed_audio=processed_filename)
